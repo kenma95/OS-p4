3211b9b08c59d08c04b5154c64c165a0d9c26bff
mrqken <kenma95@gmail.com>
2015-12-07 20:04:01 -0500

    Functional Store & Dir

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import socket               # Import socket module
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 
-s.connect(("45.63.120.174", 8765))
+s.connect(("localhost", 8765))
 print "Client: Connect Sucessful"
 print s.recv(1024)
 f = open ("cmmd.txt",'r')
--- server.py
+++ server.py
@@ -11,70 +11,109 @@ class Disk:
 
 		relation = {}
 		disk_map = "."*n_blocks
-		print "Block size is " + blocksize
-		print "Number of blocks is " + n_blocks
+		print "Block size is " + str(blocksize)
+		print "Number of blocks is " + str(n_blocks)
+
+
+def readlines(buffer,c, recv_buffer=4096, delim='\n'):
+	data = c.recv(recv_buffer)
+	buffer += data
+	lines = buffer.split('\n')
+	if buffer and buffer[-1] == '\n':		#check last command's integrity
+		buffer =lines.pop()
+	else:
+		buffer = ''
+			
+	return buffer, lines
+
 
 
 def folder_init():
 	if not os.path.exists(".storage"):
-    	os.makedirs(".storage")
+		os.makedirs(".storage")
 	else:
-		shutil.rmtree('/.storage')
+		shutil.rmtree('.storage')
 		os.makedirs(".storage")
 
 def get_thread_id():
-	return "[thread" + thread.get_ident() +"] "
+	return "[thread" + str(threading.current_thread() )+"] "
 
 
-def store(line,s):
-   	phrase_list = line.split()
+def store(line_index,c,lines):
+	print lines
+	command = lines[line_index]
+   	phrase_list = command.split()
    	if len(phrase_list) < 3:
    		print get_thread_id()+"ERROR: WRONG FORMAT"
    	file_name = phrase_list[1]
-   	file_size = phrase_list[2]
+   	file_size = int(phrase_list[2])
+
    	if os.path.exists(".storage/"+file_name):
    		print get_thread_id()+"ERROR: FILE EXISTS"
-   	f = open(file_name,'w')
+   	f = open(".storage/"+file_name,'w')
    	size_count = 0
+   	next_command_index =line_index
+   	while size_count < file_size:
+   		next_command_index +=1
+   		remain = file_size -size_count
+   		if remain > len(lines[next_command_index]):			#incoming line, or \n
+   			to_add = lines[next_command_index] + '\n'
+
+   		elif remain <= len(lines[next_command_index]):
+   			to_add = lines[next_command_index][0:remain]
+
+   		f.writelines(to_add)
+   		size_count += len(to_add)
+
    	#TODO: cluster sim
    	#===================
    	#
-   	while (size_count< file_size):
-   		buf = c.recv(1024)
-   		size_count += len(buf)
-   		f.write(buf)
-   		if (size_count < file_size):
-   			f.write("\n")
+   	
    	f.close()
-   	c.send("ACK");
-   	print get_thread_id()+"Sent ACK"
+   	#c.send("ACK");
+   	print "Sent ACK"
+   	return next_command_index
+
 
 def session(c, addr):
-	line = c.recv(1024)
+	buffer = ''
+	buffer, lines =readlines(buffer,c)
+	skip = -1
 
-	while line:
-   		print get_thread_id() + "Rcvd: " + line
-	   		if line.startswith("STORE "):
-	   			store(line,c)
-	   		if line.startswith("DIR"):
-	   			dir_(line,c)
+	while buffer or lines:
+		for i in range(0,len(lines)):
+			if i <= skip:
+				continue
+			elif lines[i].startswith("STORE "):
+				skip = store(i,c,lines)
+			elif lines[i].startswith("DIR"):
+	   			dir_(c)
+	   		else:
+	   			print "ERROR: UNDEFINED COMMAND"
 
-	   	line = c.recv(1024)
-	c.close()				#Disconnect if no more byte recv
+	   	buffer, lines =readlines(buffer,c)
+
+
+					#Disconnect if no more byte recv
+	c.close()
 	print get_thread_id() + "Client closed its socket....terminating"
 
 
 
 
-def dir_(line,c):
+
+def dir_(c):
 	file_list = os.listdir(".storage")
-	c.send(len(file_list))
-	print get_thread_id()+"Sent "+len(file_list)
-	if not file_list.empty():
+	#c.send(len(file_list))
+	print get_thread_id()+"Sent "+str(len(file_list))
+	if file_list:
 		file_list.sort()
 		for item in file_list:
-			c.send(item)
+			#c.send(item)
 			print item
+
+
+
 
 
 def main():
@@ -84,19 +123,16 @@ def main():
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	port = 8765
 	s.bind(("",port))        # Bind to the port
-	print "Listening on port "+port
+	print "Listening on port "+str(port)
 
 	s.listen(5)
 	while True:
 		c, addr = s.accept()     # Establish connection with client.
-		print "Received incoming connection from "+addr
+		print "Received incoming connection from "+str(addr)
 	   	c.send('Server:Connected successful')
 		#new thread here
 		thread = threading.Thread(target=session, args=(c, addr))
 		thread.start()	
-
-
-
 
 
 
