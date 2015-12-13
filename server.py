b34a8b7d6235c3b9e025fa24f16f154d7d99814e
mrqken <kenma95@gmail.com>
2015-12-13 16:31:55 -0500

    full of BS
    BS!

--- server.py
+++ server.py
@@ -4,31 +4,6 @@ import os
 import shutil
 
 
-class Disk:
-    """Simulation class of Cluster Disk"""
-
-    def __init__(self, n_blocks, blocksize):
-        self.n_blocks = n_blocks
-        self.blocksize = blocksize
-
-        relation = {}
-        disk_map = "." * n_blocks
-        print "Block size is " + str(blocksize)
-        print "Number of blocks is " + str(n_blocks)
-
-
-def readlines(buffer, c, recv_buffer=4096, delim='\n'):
-    data = c.recv(recv_buffer)
-    buffer += data
-    lines = buffer.split('\n')
-    if buffer and buffer[-1] == '\n':  # check last command's integrity
-        buffer = lines.pop()
-    else:
-        buffer = ''
-
-    return buffer, lines
-
-
 def folder_init():
     if not os.path.exists(".storage"):
         os.makedirs(".storage")
@@ -36,79 +11,123 @@ def folder_init():
         shutil.rmtree('.storage')
         os.makedirs(".storage")
 
-
 def get_thread_id():
-    return "[thread" + str(threading.current_thread()) + "] "
+    return "[thread" + str(threading.current_thread().ident) + "] "
+
+def read(line,c):
+    phrase_list = line.split()
+    if len(phrase_list) < 4:
+        print get_thread_id() + "Sent: ERROR: WRONG FORMAT"
+        c.send("ERROR: WRONG FORMAT")
+    file_name = phrase_list[1]
+    file_offset = int(phrase_list[2])
+    file_len = int(phrase_listp[3])
+    if not os.path.exists(".storage/" + file_name):
+        print get_thread_id + "Sent: ERROR: NO SUCH FILE"
+        c.send("ERROR: NO SUCH FILE")
+    else:
+        f = open(".storage/" + file_name, 'r')
+        file_size = os.path.getsize(file_name)
+        if file_size < (file_offset + file_len):
+            print get_thread_id() + "ERROR: INVALID BYTE RANGE" 
+        f.seek(file_offset,0)
+        to_read = f.read(file_len)
+        to_read = "ACK str(file_len)\n" + to_read
+        c.send(to_read)
+        print get_thread_id() + "Sent: "+to_read
+    #simulation
+
+
+
+
+def delete(line,c):
+    phrase_list = line.split()
+    if len(phrase_list) < 2:
+        print get_thread_id() + "ERROR: WRONG FORMAT"
+        c.send("ERROR: WRONG FORMAT")
+    file_name =  phrase_list[1]
+    if not os.path.exists(".storage/" + file_name):
+        print get_thread_id + "Sent: ERROR: NO SUCH FILE"
+        c.send("ERROR: NO SUCH FILE")
+    else:
+        os.remove(file_name)
+        print get_thread_id() + "Deleted " + file_name + " file 'A' (deallocated 7 blocks)"
+        c.send("Deleted " + file_name + " file 'A' (deallocated 7 blocks)")
+    return
 
 
 def store(line, c):
-    print lines
-    command = lines[line_index]
-    phrase_list = command.split()
+    phrase_list = line.split()
     if len(phrase_list) < 3:
         print get_thread_id() + "ERROR: WRONG FORMAT"
+        c.send("ERROR: WRONG FORMAT")
+        return
     file_name = phrase_list[1]
     file_size = int(phrase_list[2])
-
+    # TODO: cluster sim
+    #
+    '''
+    file_id = disk.store(file_name,file_size)
+    print get_thread_id() + "Simulated Clustered Disk Space Allocation:"
+    print disk
+    '''
     if os.path.exists(".storage/" + file_name):
+        c.send("ERROR: FILE EXISTS")
         print get_thread_id() + "ERROR: FILE EXISTS"
+        return
     f = open(".storage/" + file_name, 'w')
     size_count = 0
-    next_command_index = line_index
-    while size_count < file_size:
-        next_command_index += 1
-        remain = file_size - size_count
-        if remain > len(lines[next_command_index]):  # incoming line, or \n
-            to_add = lines[next_command_index] + '\n'
-
-        elif remain <= len(lines[next_command_index]):
-            to_add = lines[next_command_index][0:remain]
-
-        f.writelines(to_add)
+    while (size_count<file_size):
+        c.send(" ")
+        to_add = c.recv(1024)
         size_count += len(to_add)
+        if (size_count < file_size):
+            to_add+="\n"
+            size_count += 1
+        f.write(to_add)
+    c.send("ACK")
+    print get_thread_id() +"Sent: ACK"
 
-    # TODO: cluster sim
-    # ===================
-    #
 
     f.close()
     # c.send("ACK");
-    return next_command_index
+    return
 
 
 def session(c, addr):
-    while 1:
-        line = c.recv(1024)
-        if len(line) == 0:  #nothing receive, connection over
-            break
-        print [thread 134558720] Rcvd: STORE abc.txt 25842
-        else if line.startswith("STORE "):
-            store(line,c)
-            c.send("ACK")
-        elif line.startswith("DIR"):
-            dir_(c)
-            print "DIR"
-        else:
-            print "ERROR: UNDEFINED COMMAND"
-            break
+    try:
+        while 1:
+            line = c.recv(1024)
+            if len(line) == 0:  #nothing receive, connection over
+                break
+            print get_thread_id() + " Rcvd: " + line
+            if line.startswith("STORE "):
+                store(line,c)
+            elif line.startswith("DIR"):
+                dir_(c)
+            else:
+                print "ERROR: UNDEFINED COMMAND"
+                break
 
-        #buffer, lines = readlines(buffer, c)
+            #buffer, lines = readlines(buffer, c)
 
-
-    # Disconnect if no more byte recv
-    c.close()
-    print get_thread_id() + "Client closed its socket....terminating"
+        # Disconnect if no more byte recv
+        c.close()
+        print get_thread_id() + "Client closed its socket....terminating"
+    except KeyboardInterrupt:
+        return
 
 
 def dir_(c):
     file_list = os.listdir(".storage")
-    # c.send(len(file_list))
+    to_return = (str(len(file_list)))
     print get_thread_id() + "Sent " + str(len(file_list))
     if file_list:
         file_list.sort()
         for item in file_list:
-            # c.send(item)
+            to_return += (item)
             print item
+    c.send(to_return)
 
 
 def main():
