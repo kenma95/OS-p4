23a43ce1155761744d74cc3bc2fe53ed9b4410e5
Yunang Chen <cya.tempsperdu@gmail.com>
2015-12-14 16:12:49 -0500

    Disk

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python           # This is client.py file
 
-import socket               # Import socket module
+import socket  # Import socket module
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
+s = socket.socket()  # Create a socket object
+host = socket.gethostname()  # Get local machine name
 
 s.connect(("localhost", 8765))
 
 print "Client: Connect Sucessful"
 print s.recv(1024)
-f = open ("cmmd.txt",'r')
+f = open("cmmd.txt", 'r')
 for line in f:
-	s.send(line)
-	if  line.startswith("STORE") or  line.startswith("DIR") or \
-	line.startswith("READ") or  line.startswith("DEL"):
-		re = s.recv(1024)
-		print re
-s.close                     # Close the socket when done
+    if not line.startswith("END"):
+        s.send(line)
+    else:
+        re = s.recv(1024)
+        print re
+s.close()  # Close the socket when done
