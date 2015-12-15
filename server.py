ce8892e937ddd17f6a60d944038d823b1546e57b
Yunang Chen <cya.tempsperdu@gmail.com>
2015-12-15 07:07:48 -0500

    bugs fixed

--- server.py
+++ server.py
@@ -2,7 +2,9 @@ import socket
 import threading
 import os
 import shutil
-import disk
+from disk import Disk
+
+
 def folder_init():
     if not os.path.exists(".storage"):
         os.makedirs(".storage")
@@ -10,10 +12,12 @@ def folder_init():
         shutil.rmtree('.storage')
         os.makedirs(".storage")
 
-def get_thread_id():
-    return "[thread" + str(threading.current_thread().ident) + "] "
 
-def read(line,c,disk):
+def get_thread_id():
+    return "[thread " + str(threading.current_thread().ident) + "] "
+
+
+def read(line, c, disk):
     phrase_list = line.split()
     if len(phrase_list) < 4:
         print get_thread_id() + "Sent: ERROR: WRONG FORMAT"
@@ -22,48 +26,49 @@ def read(line,c,disk):
     file_offset = int(phrase_list[2])
     file_len = int(phrase_list[3])
     if not os.path.exists(".storage/" + file_name):
-        print get_thread_id + "Sent: ERROR: NO SUCH FILE"
-        c.send("ERROR: NO SUCH FILE")
+        print get_thread_id() + "Sent: ERROR: NO SUCH FILE"
+        c.send("ERROR: NO SUCH FILE\n")
     else:
         f = open(".storage/" + file_name, 'r')
         file_size = os.path.getsize(".storage/" + file_name)
         if file_size < (file_offset + file_len):
             print get_thread_id() + "ERROR: INVALID BYTE RANGE"
-        f.seek(file_offset,0)
+        f.seek(file_offset, 0)
         to_read = f.read(file_len)
-        to_read = "ACK\n" + str(file_len)+"\n" + to_read +"\n"
+        to_read = "ACK\n" + str(file_len) + "\n" + to_read + "\n"
         c.send(to_read)
-        print get_thread_id() + "Sent: "+to_read
-        file_id, block_num = disk.read(file_name,file_offset,file_len)
-        print "Sent " + str(file_len) +" bytes (from "+str(block_num) +" '"+file_id+"' blocks) from offset "+str(file_offset)
+        print get_thread_id() + "Sent: ACK", file_len
+        file_id, block_num = disk.read(file_name, file_offset, file_len)
+        print get_thread_id() + "Sent " + str(file_len) + " bytes (from " + str(block_num) + " '" + file_id \
+              + "' blocks) from offset " + str(file_offset)
 
 
 def dir_(c):
     file_list = os.listdir(".storage")
-    to_return = (str(len(file_list)))+"\n"
+    to_return = (str(len(file_list))) + "\n"
     print get_thread_id() + "Sent " + str(len(file_list))
     if file_list:
         file_list.sort()
         for item in file_list:
-            to_return += (item) +"\n"
+            to_return += item + "\n"
             print item
     c.send(to_return)
 
 
-
-def delete(line,c,disk):
+def delete(line, c, disk):
     phrase_list = line.split()
     if len(phrase_list) < 2:
         print get_thread_id() + "ERROR: WRONG FORMAT"
         c.send("ERROR: WRONG FORMAT\n")
-    file_name =  phrase_list[1]
+    file_name = phrase_list[1]
     if not os.path.exists(".storage/" + file_name):
-        print get_thread_id + "Sent: ERROR: NO SUCH FILE"
+        print get_thread_id() + "Sent: ERROR: NO SUCH FILE"
         c.send("ERROR: NO SUCH FILE\n")
     else:
-        file_id,num_block = disk.delete(file_name)
+        file_id, num_block = disk.delete(file_name)
         os.remove(".storage/" + file_name)
-        print get_thread_id() + "Deleted " + file_name + " file '"+file_id+"' (deallocated "+str(num_block) +" blocks)"
+        print get_thread_id() + "Deleted " + file_name + " file '" + file_id + "' (deallocated " + str(
+                num_block) + " blocks)"
         print get_thread_id() + "Simulated Clustered Disk Space Allocation:"
         print disk
         print get_thread_id() + "Sent: ACK"
@@ -71,7 +76,7 @@ def delete(line,c,disk):
     return
 
 
-def store(line, c,disk):
+def store(line, c, disk):
     phrase_list = line.split()
     if len(phrase_list) < 3:
         print get_thread_id() + "ERROR: WRONG FORMAT"
@@ -79,7 +84,7 @@ def store(line, c,disk):
         return
     file_name = phrase_list[1]
     file_size = int(phrase_list[2])
-    file_id, block, cluster = disk.store(file_name,file_size)
+    file_id, block, cluster = disk.store(file_name, file_size)
     if file_id is None:
         print get_thread_id() + "ERROR: INSUFFICIENT DISK SPACE"
         c.send("ERROR: ERROR: INSUFFICIENT DISK SPACE\n")
@@ -94,37 +99,35 @@ def store(line, c,disk):
     size_count = len(to_add)
     if not size_count == file_size:
         print get_thread_id() + "ERROR: FILE SIZE NOT MATCH"
-        c.send("ERROR: FILE SIZE NOT MATCH")
+        c.send("ERROR: FILE SIZE NOT MATCH\n")
     f.write(str(to_add))
     f.close()
-    print "%s Stored file '%s' (%s bytes; %d blocks; %d cluster)"%(get_thread_id(),file_size,file_id,block,cluster)
+    print get_thread_id() + "Stored file '%s' (%d bytes; %d blocks; %d clusters)" % (file_id, file_size, block, cluster)
     print get_thread_id() + "Simulated Clustered Disk Space Allocation:"
     print disk
     c.send("ACK\n")
-    print get_thread_id() +"Sent: ACK"
+    print get_thread_id() + "Sent: ACK"
     return
 
 
-def session(c, addr,disk):
+def session(c, addr, disk):
     try:
         while 1:
             line = c.recv(1024)
-            if len(line) == 0:  #nothing receive, connection over
+            if len(line) == 0:  # nothing receive, connection over
                 break
-            print get_thread_id() + " Rcvd: " + line
+            print get_thread_id() + "Rcvd: " + str(line).rstrip()
             if line.startswith("STORE "):
-                store(line,c,disk)
+                store(line, c, disk)
             elif line.startswith("DIR"):
                 dir_(c)
             elif line.startswith("READ "):
-                read(line,c,disk)
-            elif line.startswith("DEL "):
-                delete(line,c,disk)
+                read(line, c, disk)
+            elif line.startswith("DELETE "):
+                delete(line, c, disk)
             else:
                 print "ERROR: UNDEFINED COMMAND"
                 break
-
-            #buffer, lines = readlines(buffer, c)
 
         # Disconnect if no more byte recv
         c.close()
@@ -135,7 +138,12 @@ def session(c, addr,disk):
 
 def main():
     folder_init()
-    d = disk.Disk(128, 4096)
+    n_blocks = 128
+    blocksize = 4096
+    d = Disk(n_blocks, blocksize)
+    print "Block size is", blocksize
+    print "Number of blocks is", n_blocks
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     port = 8765
     s.bind(("", port))  # Bind to the port
@@ -146,9 +154,9 @@ def main():
         try:
             c, addr = s.accept()  # Establish connection with client.
             print "Received incoming connection from " + str(addr)
-            c.send('Server:Connected successful\n')
+            c.send('Server: Connected successful\n')
             # new thread here
-            thread = threading.Thread(target=session, args=(c, addr,d))
+            thread = threading.Thread(target=session, args=(c, addr, d))
             thread.start()
         except KeyboardInterrupt:
             exit()
